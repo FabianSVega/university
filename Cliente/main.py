e3002b05ce43360596a97ade1219c396787b3331
FabianSVega <ffsegurav@correo.udistrital.edu.co>
2022-10-01 18:16:40 -0500

    changes 1

--- Cliente/main.py
+++ Cliente/main.py
@@ -1,39 +1,41 @@
 #Autores: Fabian Segura, Fernando Gomez
-# Fecha Actualizacion: 28/08/2022 
-from kivy.lang					import Builder
-from kivymd.app					import MDApp
-import speech_recognition       as sr
-from kivy.core.window 			import Window
-from turtle 					import *
-from turtle 					import TurtleScreen
-# Librerias funcionales
-import os
-import turtle
-import socket
-import threading
-import ast
-import turtle
-import time
-import RPi.GPIO as GPIO
-Window.fullscreen = 0
-Window.size = (500,500)
+# Fecha Actualizacion: 01/10/2022 
 
+from kivy.lang					import Builder
+from kivy.core.window 			import Window
+from kivymd.app					import MDApp
+from turtle 					import * 
+import speech_recognition       as sr
+
+#_____________	Librerias funcionales	_______________________
+import os
+import ast
+import time
+import socket
+import turtle
+import threading
+import RPi.GPIO as GPIO
+
+#____________	Variables Globales	___________________________
 pin	 = 10
 pin2 = 12
+Window.fullscreen = 0
+Window.size = (500,500)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin,GPIO.OUT)
 GPIO.setup(pin2,GPIO.OUT)
 
 class MainApp(MDApp):
-	screens	= {}
-	lista = []
-	fraseDicha = None
+	screens		= {}
+	lista		= []
+	fraseDicha 	= None
+ 
 	def build(self):
 		self.theme_cls.primary_palette	= "DeepPurple"
 		self.principal()
 
 	def go_screen(self, screenname):	
-		self.root.ids.sm.switch_to(self.load_screen(screenname), direction	= 'left', duration=.07)  #Ir a la vista
+		self.root.ids.sm.switch_to(self.load_screen(screenname), direction	= 'left', duration=.07)  
 
 	def load_screen(self, screenname):
 		self.view	= screenname
@@ -62,7 +64,9 @@ Screen:
 	def thread_voice(self):
 		threading.Thread(target=self.reconocimiento).start()
 
+	#Funcion para el aviso de busqueda de sonido
 	def led(self):
+     
 		timew=0
 		led=True
 		while led:
@@ -72,30 +76,37 @@ Screen:
 			GPIO.output(pin,GPIO.LOW)
 			GPIO.output(pin2,GPIO.LOW)
 			time.sleep(.05)
-			timew=1+timew
-			print(timew)
-			if timew ==40:
+			timew	= 1+timew
+   
+			if timew == 40:
 				led=False
-
-	def reconocimiento(self):	
-		micro = sr.Microphone()   
-		r = sr.Recognizer()
+    
+	# Funcion reconocimiento de palabras
+	def reconocimiento(self):
+     	
+		micro 	= sr.Microphone()   
+		r		= sr.Recognizer()
+  
 		while True:
+      
 			with micro as source:
 				threading.Thread(target=self.led).start()
 				print('Porfavor hable fuerte y claro')
 				self.screens['principal'].ids.dato.text='!!Porfavor hable fuerte y claro¡¡'
 				r.adjust_for_ambient_noise(source)
 				audio = r.listen(source)
+    
 				try:
 					self.fraseDicha = r.recognize_google(audio,language='es-CO')
 					print(self.fraseDicha)
+     
 				except:
 					with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as mysv:
 						mysv.connect(("169.254.231.179", 10000))
 						mysv.send("incapaz_de_reconocer".encode('utf-8'))
 						self.screens['principal'].ids.dato.text='Incapaz de reconocer'
 						self.reconocimiento()
+      
 				if self.fraseDicha == "Ayuda":
 					with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as mysv:
 						mysv.connect(("169.254.231.179", 10000))
@@ -150,15 +161,19 @@ Screen:
 						mysv.send("".encode('utf-8'))
 						self.screens['principal'].ids.dato.text='Incapaz de reconocer'
 
+	# Funcion para dibujar la poscision del robot
 	def draw_vector(self):
+     
 		while True:
 			mysv = socket.socket()
 			mysv.connect(("169.254.231.179", 3200))
 			respuesta = mysv.recv(1024)
 			x = respuesta.decode('utf-8')
 			print(x)
+   
 			if x == None or x == "" or x == []:
 				print("Ha ocurrido un error")
+    
 			else:
 				self.lista = ast.literal_eval(x)
 				print(self.lista)
@@ -169,10 +184,12 @@ Screen:
 		title("Movimiento ev3")
 		self.square()
 		self.cross()
-		turtle.speed(speed=1) 
+		turtle.speed(speed=1)
+   
 		for x in self.lista:
 			turtle.speed(speed=1)
 			turtle.pencolor("red")
+   
 			if x== 90:
 				turtle.right(90)
 			elif x== -90:
@@ -189,14 +206,15 @@ Screen:
 				turtle.forward(15)
 			elif x== 500:
 				turtle.forward(50)
-			
 			elif x==12: 
 				positiont=turtle.position()
 				print(positiont)
 				turtle.write(str(positiont), True, align="center")
 				exitonclick()
-
+    
+	# Funcion para dibujar la cruz
 	def square(self):
+     
 		i=1
 		turtle.pencolor("white")
 		turtle.speed(speed=1000)
@@ -213,10 +231,10 @@ Screen:
 			turtle.left(-180)
 			turtle.forward (720)
 		i=1  
-			
 		turtle.penup()
 		turtle.home()
 		turtle.pendown() 
+  
 		for i in range (8):
 			turtle.penup()
 			turtle.setpos(0,-40*i) 
@@ -228,12 +246,12 @@ Screen:
 			turtle.forward (360)
 			turtle.left(-180)
 			turtle.forward (720)
-		i=1 
-			
+		i=1 	
 		turtle.penup()
 		turtle.home()
 		turtle.pendown() 
 		turtle.left(90)
+  
 		for i in range (10):
 			turtle.penup()
 			turtle.setpos(40*i,0) 
@@ -246,12 +264,12 @@ Screen:
 			turtle.left(-180)
 			turtle.forward (560)
 
-		i=1
-			
+		i=1	
 		turtle.penup()
 		turtle.home()
 		turtle.pendown() 
 		turtle.left(90)
+  
 		for i in range (10):
 			turtle.penup()
 			turtle.setpos(-40*i,0) 
@@ -263,14 +281,14 @@ Screen:
 			turtle.forward (280)
 			turtle.left(-180)
 			turtle.forward (560)
-			
-		
-			
+
 		turtle.penup()
 		turtle.home()
 		turtle.pendown()
 
+	# Funcion para dibujar la cuadricula
 	def cross(self):
+     
 		turtle.shape("turtle")
 		turtle.color("white", "red")
 		turtle.pencolor("green")
